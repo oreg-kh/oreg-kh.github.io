c869c6f997e133075d66f4cf5bd994c5c63e98e9
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2024-03-03 10:50:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // index.js
 // Importáljuk a functions.js fájlt
-const { helloWorld } = require('./functions');
+const { helloWorld } = require('./html');
 
 // Most meghívhatjuk a helloWorld funkciót
 helloWorld();
