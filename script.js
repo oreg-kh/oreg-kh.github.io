9999af4d837937366138c45c1920fae32936ba43
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2024-03-03 11:29:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-import { helloWorld } from './function.js';
-
-helloWorld();
+fetch('function.js') // Betöltjük a function.js tartalmát
+  .then(response => response.text())
+  .then(text => {
+    // A betöltött kód text formátumban van, így ki kell értékelni
+    eval(text); // Az eval függvény értékeli ki a szöveget
+    helloWorld(); // Most már hívható a helloWorld függvény
+  })
+  .catch(error => console.error(error));
